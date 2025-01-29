3893eca1c32babca6c20847125c6f95edbd74694
dhasarathan <dhasasube123@gmail.com>
2025-01-29 18:03:03 +0530

    frist app

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,RouterModule,LoginComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ownProduct';
+}
