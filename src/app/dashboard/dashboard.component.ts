7c7b5d69d3c79873b1d5386d6be5ada43fee4ff9
dhasarathan <dhasasube123@gmail.com>
2025-02-06 16:20:53 +0530

    navigation added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,30 +1,18 @@
 import { Component } from '@angular/core';
-import { navigation } from '../data/navigation';
 import { Router } from '@angular/router';
+import { navigation } from '../data/navigation';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  constructor(private router: Router) {}
-
-  navigation_list = navigation.map(item => ({ ...item, showSubmenu: false }));;
+  navigation_list = navigation.map(item => ({ ...item, showSubmenu: false }));
   isExpanded = true;
-  showSubmenu: boolean = false;
   isShowing = false;
-  showSubSubMenu: boolean = false;
 
-  logout() {
-    // Implement your logout logic here
-    // For example, clear user session and navigate to the login page
-    this.router.navigate(['/login']);
-  }
-  toggleSubmenu(item: any) {
-    // Toggle the submenu visibility when the parent is clicked
-    item.showSubmenu = !item.showSubmenu;
-  }
+  constructor(private router: Router) {}
 
   mouseenter() {
     if (!this.isExpanded) {
@@ -36,5 +24,19 @@ export class DashboardComponent {
     if (!this.isExpanded) {
       this.isShowing = false;
     }
+  }
+
+  toggleSubmenu(item: { showSubmenu: boolean; }) {
+    item.showSubmenu = !item.showSubmenu;
+  }
+
+  toggleSidenav() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  logout() {
+    // Implement your logout logic here
+    // For example, clear user session and navigate to the login page
+    this.router.navigate(['/login']);
   }
 }
