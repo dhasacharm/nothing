27b2405d40d29c1753ada4cf61c3e5b2002d2073
dhasarathan <dhasasube123@gmail.com>
2025-02-12 11:40:20 +0530

    test

---
+++ src/app/data/fullRegister.ts
@@ -0,0 +1,11 @@
+export const RegisterForm = [
+    {
+        "lable": "Email",
+        "type": "email",
+        "class": "form-control",
+        "name": "email",
+        "placeholder": "Enter Your Email",
+        "required": true,
+        "dropdownValue": [{ "label": "test", value: "test" }]
+    }
+]
